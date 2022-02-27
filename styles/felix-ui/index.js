e0eb0a4ad817231a06278fc644d1c8516c67eb37
Animesh garai <animeshgarai09@gmail.com>
2022-02-27 10:07:42 +0530

    bug fix

--- styles/felix-ui/index.js
+++ styles/felix-ui/index.js
@@ -2,7 +2,7 @@ import Avatar from "./components/Avatar/Avatar";
 import AvatarGroup from "./components/Avatar/AvatarGroup";
 import Alert from "./components/Alert/Alert";
 import Button from "./components/Button/Button";
-import ButtonGroup from "./components/Button/buttonGroup";
+import ButtonGroup from "./components/Button/ButtonGroup";
 import ProductCard from "./components/Card/ProductCard";
 import Image from "./components/Image";
 import A from './components/A';
