3723b8ae5ce9b9bc3ca2095d862d0619e0626d3b
Animesh garai <animeshgarai09@gmail.com>
2022-02-27 10:04:59 +0530

    Button groups added

--- styles/felix-ui/components/Button/Button.js
+++ styles/felix-ui/components/Button/Button.js
@@ -1,12 +1,14 @@
+import PropTypes from 'prop-types'
 import styles from './button.module.scss'
 
 const Button = (
     {
         size = "md", //'xs' || 'sm' || 'md' || 'lg', // md is by default
-        theme = "primary", //'primary' || 'info' || 'warning' || 'success' || disable, // primary is by default
+        theme = "primary", //'primary' || 'info' || 'warning' || 'success' || 'gray',|| disable, // primary is by default
         variant = "solid", //['round','outline','ghost','link'], // solid is by default
         leftIcon, // React-icons
         rightIcon, // React-icons
+        transform = true, // Transform on hover
         className, //user-defined classnames
         children //button text
     }) => {
@@ -16,6 +18,7 @@ const Button = (
     let classNames = [styles.btn, styles[size], styles[theme]]
     classNames = classNames.concat((typeof (variant) == 'string' ? [styles[variant]] || '' : variant.map((item) => styles[item])))
     classNames.push((leftIcon || rightIcon) && !children ? styles.icon : '')
+    classNames.push(!transform && styles.noTransform)
     classNames.push(className)
     return (
         <button type='button' className={`${classNames.join(' ')}`}>
@@ -24,6 +27,20 @@ const Button = (
             {rightIcon}
         </button>
     )
+}
+
+Button.propTypes = {
+    size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+    theme: PropTypes.oneOf(['primary', 'info', 'warning', 'success', 'gray', 'disable']),
+    variant: PropTypes.oneOfType([
+        PropTypes.oneOf(['round', 'outline', 'ghost', 'link']),
+        PropTypes.arrayOf(PropTypes.oneOf(['round', 'outline', 'ghost', 'link']))
+    ]),
+    leftIcon: PropTypes.element,
+    rightIcon: PropTypes.element,
+    transform: PropTypes.bool,
+    className: PropTypes.string,
+    children: PropTypes.node
 }
 
 export default Button
--- styles/felix-ui/index.js
+++ styles/felix-ui/index.js
@@ -2,10 +2,24 @@ import Avatar from "./components/Avatar/Avatar";
 import AvatarGroup from "./components/Avatar/AvatarGroup";
 import Alert from "./components/Alert/Alert";
 import Button from "./components/Button/Button";
+import ButtonGroup from "./components/Button/buttonGroup";
 import ProductCard from "./components/Card/ProductCard";
 import Image from "./components/Image";
 import A from './components/A';
 import Badge from "./components/Badge/Badge";
 import IconBadge from "./components/IconBadge/IconBadge";
 import Input from './components/Input/Input'
-export { A, Avatar, AvatarGroup, Alert, Button, Badge, ProductCard, IconBadge, Image, Input }
+
+export {
+    A,
+    Avatar,
+    AvatarGroup,
+    Alert,
+    Button,
+    ButtonGroup,
+    Badge,
+    ProductCard,
+    IconBadge,
+    Image,
+    Input
+}
